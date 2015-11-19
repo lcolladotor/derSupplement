cb2abb74f52a3c52d281c235bbb53b85e9d54f3c
lcolladotor <lcollado@jhsph.edu>
2015-11-19 14:27:24 -0500

    Generate reads for simulation

---
+++ simulation/generateReads/render.sh
@@ -0,0 +1,14 @@
+#!/bin/bash	
+#$ -cwd
+#$ -m e
+#$ -l mem_free=20G,h_vmem=30G,h_fsize=20G
+#$ -N render-genReads
+echo "**** Job starts ****"
+date
+
+# Generate HTML
+module load R/devel
+Rscript -e "rmarkdown::render('generateReads.Rmd')"
+
+echo "**** Job ends ****"
+date
