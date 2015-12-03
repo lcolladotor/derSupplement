a5393bc2a5cbde63a2b685cf1a48236634fb1304
lcolladotor <lcollado@jhsph.edu>
2015-12-02 19:10:15 -0500

    Copy scripts to run featureCounts with incomplete GTF

---
+++ simulation/featurecounts-inc/run-featCounts-inc.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Directories
+MAINDIR=/dcs01/ajaffe/Brain/derRuns/derSupplement/simulation
+WDIR=${MAINDIR}/featurecounts-inc
+
+# Define variables
+CORES=4
+
+mkdir -p ${WDIR}
+mkdir -p ${WDIR}/logs
+
+sname="featCounts-inc"
+        
+## Create script
+cat > ${WDIR}/.${sname}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -m e
+#$ -l mem_free=3G,h_vmem=4G,h_fsize=10G
+#$ -pe local ${CORES}
+#$ -N ${sname}
+
+echo "**** Job starts ****"
+date
+
+cd ${WDIR}
+
+
+## Load R
+module load R/devel
+
+## Run featureCounts
+Rscript featureCounts-inc.R
+
+mv ${WDIR}/${sname}.* ${WDIR}/logs/
+
+echo "**** Job ends ****"
+date
+
+EOF
+
+call="qsub ${WDIR}/.${sname}.sh"
+echo $call
+$call
