1f78f546d9bde37d840d5f9f57475bb2c77e5ad2
lcolladotor <lcollado@jhsph.edu>
2016-02-11 11:20:58 -0500

    Run stringtie by replicate instead of all at once

--- simulation/stringtie/run-stringtie.sh
+++ simulation/stringtie/run-stringtie.sh
@@ -11,20 +11,24 @@ CORES=4
 mkdir -p ${WDIR}
 mkdir -p ${WDIR}/logs
 
-touch ${WDIR}/GTFfiles.txt
+touch ${WDIR}/GTFfiles-R1.txt
+touch ${WDIR}/GTFfiles-R2.txt
+touch ${WDIR}/GTFfiles-R3.txt
 
-ls ${DATADIR}/*[0-9].bam | while read fullpath
-    do
-    bamfile=$(basename "${fullpath}")
-    libname="${bamfile%.*}"
-    echo "Creating script for ${libname}"
-    sname="${libname}.stringtie"
+for replicate in 1 2 3
+    do 
+    ls ${DATADIR}/*R${replicate}.bam | while read fullpath
+        do
+        bamfile=$(basename "${fullpath}")
+        libname="${bamfile%.*}"
+        echo "Creating script for ${libname}"
+        sname="${libname}.stringtie"
     
-    ## Create GTF file list for cuffmerge
-    echo "${WDIR}/${libname}/outfile.gtf" >> ${WDIR}/GTFfiles.txt
+        ## Create GTF file list for cuffmerge
+        echo "${WDIR}/${libname}/outfile.gtf" >> ${WDIR}/GTFfiles-R${replicate}.txt
     
-    ## Create scripts    
-	cat > ${WDIR}/.${sname}.sh <<EOF
+        ## Create scripts    
+    	cat > ${WDIR}/.${sname}.sh <<EOF
 #!/bin/bash
 #$ -cwd
 #$ -m e
@@ -52,16 +56,15 @@ echo "**** Job ends ****"
 date
 
 EOF
-	call="qsub ${WDIR}/.${sname}.sh"
-	echo $call
-	$call
-done
+    	call="qsub ${WDIR}/.${sname}.sh"
+    	echo $call
+    	$call
+    done
 
 
-## Now run cuffmerge
-
-sname="cmerge-stie"
-cat > ${WDIR}/.${sname}.sh <<EOF
+    ## Now run cuffmerge
+    sname="cmerge-stie-R${replicate}"
+    cat > ${WDIR}/.${sname}.sh <<EOF
 #!/bin/bash
 #$ -cwd
 #$ -m e
@@ -78,11 +81,14 @@ cd ${WDIR}
 ## Load Cuffmerge
 module load cufflinks/2.2.1
 
+## Subset GTF files
+grep "R${replicate}" ${WDIR}/GTFfiles.txt > ${WDIR}/GTFfiles-R${replicate}.txt
+
 ## Cuffmerge version usedd
 cuffmerge --version
 
 ## Run cuffmerge
-cuffmerge -o ${WDIR}/cuffmerge -p ${CORES} -g ${MAINDIR}/gtf/chr17.gtf ${WDIR}/GTFfiles.txt
+cuffmerge -o ${WDIR}/cuffmerge-R${replicate} -p ${CORES} -g ${MAINDIR}/gtf/chr17.gtf ${WDIR}/GTFfiles-R${replicate}.txt
 
 mv ${WDIR}/${sname}.* ${WDIR}/logs/
 
@@ -90,26 +96,60 @@ echo "**** Job ends ****"
 date
 EOF
 
-call="qsub ${WDIR}/.${sname}.sh"
-echo $call
-$call
+    call="qsub ${WDIR}/.${sname}.sh"
+    echo $call
+    $call
 
-ls ${DATADIR}/*[0-9].bam | while read fullpath
-    do
-    bamfile=$(basename "${fullpath}")
-    libname="${bamfile%.*}"
-    echo "Creating script for ${libname}"
-    sname="${libname}.bgprep"
+## Next, cuffcompare
+    sname="cuffcomp-stie-R${replicate}"
+    cat > ${WDIR}/.${sname}.sh <<EOF
+#!/bin/bash
+#$ -cwd
+#$ -m e
+#$ -l mem_free=3G,h_vmem=4G,h_fsize=10G
+#$ -N ${sname}
+#$ -hold_jid cmerge-stie-R${replicate}
+
+echo "**** Job starts ****"
+date
+
+cd ${WDIR}
+
+## Load cuffcompare
+module load cufflinks/2.2.1
+
+## Create dir for results
+mkdir -p cuffcompare
+
+## Run cuffcompare
+cuffcompare -r ${MAINDIR}/gtf/chr17.gtf -o cuffcompare/cuffcomp-R${replicate} -V ${WDIR}/cuffmerge-R${replicate}/merged.gtf
+
+mv ${WDIR}/${sname}.* ${WDIR}/logs/
+
+echo "**** Job ends ****"
+date
+EOF
+
+    call="qsub ${WDIR}/.${sname}.sh"
+    echo $call
+    $call
+
+    ls ${DATADIR}/*R${replicate}.bam | while read fullpath
+        do
+        bamfile=$(basename "${fullpath}")
+        libname="${bamfile%.*}"
+        echo "Creating script for ${libname}"
+        sname="${libname}.bgprep"
         
-    ## Create scripts    
-	cat > ${WDIR}/.${sname}.sh <<EOF
+        ## Create scripts    
+    	cat > ${WDIR}/.${sname}.sh <<EOF
 #!/bin/bash
 #$ -cwd
 #$ -m e
 #$ -l mem_free=2G,h_vmem=4G,h_fsize=10G
 #$ -pe local ${CORES}
 #$ -N ${sname}
-#$ -hold_jid cmerge-stie
+#$ -hold_jid cmerge-stie-R${replicate}
 
 echo "**** Job starts ****"
 date
@@ -120,7 +160,7 @@ cd ${WDIR}
 stringtie --version
 
 ## Run StringTie
-stringtie ${fullpath} -o ${WDIR}/${libname}/outfile-run2.gtf -p ${CORES} -G ${WDIR}/cuffmerge/merged.gtf -b ${WDIR}/${libname}/ -e
+stringtie ${fullpath} -o ${WDIR}/${libname}/outfile-run2.gtf -p ${CORES} -G ${WDIR}/cuffmerge-R${replicate}/merged.gtf -b ${WDIR}/${libname}/ -e
 
 mv ${WDIR}/${sname}.* ${WDIR}/logs/
 
@@ -128,8 +168,10 @@ echo "**** Job ends ****"
 date
 
 EOF
-	call="qsub ${WDIR}/.${sname}.sh"
-	echo $call
-	$call
+    	call="qsub ${WDIR}/.${sname}.sh"
+    	echo $call
+    	$call
+    done
+
 done
 
