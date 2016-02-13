e0599b06a86a22115ae780cefdffa93b30c6b137
lcolladotor <lcollado@jhsph.edu>
2016-02-13 15:43:47 -0500

    Change ballgown pipeline. Manual tests releaved that using meas='cov' instead of meas='FPKM' yields better results at the transcript level. Also now testing at the exon level and saving results in a way that will make it easier in the evaluate script

--- simulation/ballgown/ballgown_analysis.R
+++ simulation/ballgown/ballgown_analysis.R
@@ -34,12 +34,31 @@ print(object.size(bg), units = 'Mb')
 ## Save ballgown object
 save(bg, file = paste0('bg-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
 
-## Perform statistical tests
-stat_results <- stattest(gown = bg, feature = 'trans', getFC = TRUE, meas = 'FPKM', covariate = 'group')
+## Perform statistical tests: transcript level
+stat_results <- stattest(gown = bg, feature = 'trans', meas = 'cov', covariate = 'group')
 
+## Create nice GRangesList: transcript level
+bgres <- structure(bg)$trans
+mcols(bgres) <- cbind(mcols(bgres), stat_results)
 
 ## Save results
-save(stat_results, file = paste0('stat-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
+save(stat_results, file = paste0('stat-trans-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
+save(bgres, file = paste0('bgres-trans-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
+
+## Clean up
+rm(stat_results, bgres)
+
+## Perform statistical tests: exon level
+exon_cov  <- eexpr(bg, 'cov')
+stat_results <- stattest(gowntable = exon_cov, feature = 'exon', pData = pData(bg), mod = model.matrix(~ pData(bg)$group), mod0 = model.matrix( ~ 0 + rep(1, nrow(pData(bg)))))
+
+## Create nice GRanges (exon level)
+bgres <- structure(bg)$exon
+mcols(bgres) <- cbind(mcols(bgres), stat_results)
+
+## Save results
+save(stat_results, file = paste0('stat-exon-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
+save(bgres, file = paste0('bgres-exon-R', opt$replicate, '-', ifelse(opt$complete == 'yes', 'comp', 'inc'), '.Rdata'))
 
 
 ## Reproducibility info
