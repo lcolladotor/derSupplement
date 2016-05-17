4ead6032a1494786ce11478907182331a80445e7
lcolladotor <lcollado@jhsph.edu>
2016-05-17 11:46:05 -0400

    Just save the DERs, not the ERs

--- additional-analyses/export_bed.R
+++ additional-analyses/export_bed.R
@@ -19,11 +19,46 @@ pd_sub <- pdSpan[, c('gender', 'lab', 'Age', 'structure_acronym', 'structure_nam
 ## Save pheno data subset
 write.table(pd_sub, file = 'BrainSpan_bigwig_files.tsv', row.names = FALSE, sep = '\t', quote = FALSE)
 
+## Load models
+load('/dcl01/lieber/ajaffe/derRuns/derSupplement/brainspan/derAnalysis/run5-v1.5.30/models.Rdata')
+stopifnot(nrow(models$mod) == nrow(models$mod0))
+if(unique(sapply(models, nrow)) == 487) {
+    models$mod <- models$mod[-bad_samples, ]
+    models$mod0 <- matrix(models$mod0[-bad_samples, ], ncol = 1)
+}
+stopifnot(nrow(models$mod) == 484)
 
+## Load regions
 load('/dcl01/lieber/ajaffe/derRuns/derSupplement/brainspan/regionMatrix/regionMat-cut0.25.Rdata')
 regList <- lapply(regionMat, function(x) x$regions)
 fullRegionGR <- unlist(GRangesList(regList))
-export(fullRegionGR, 'BrainSpan_ER_DERs_cutoff_0.25.bed', format = 'BED')
+
+# coverage matrix
+fullRegionMat = do.call('rbind',
+	lapply(regionMat, function(x) x$coverageMatrix))
+## Drop bad samples
+if(ncol(fullRegionMat) == 487) fullRegionMat <- fullRegionMat[, -bad_samples]
+
+## drop regions shorter than 6 bp
+keepIndex <- which(width(fullRegionGR) >= 6)
+fullRegionGR <- fullRegionGR[keepIndex]
+fullRegionMat <- fullRegionMat[keepIndex,]
+
+## DE analysis
+fit = lmFit(y, models$mod)
+fit0 = lmFit(y, models$mod0)
+ff = getF(fit,fit0, y)
+
+print('Number and percent of ER-level DERs that are significant')
+sum(p.adjust(ff$f_pval, 'bonf') < 0.05)
+mean(p.adjust(ff$f_pval, 'bonf') < 0.05) * 100
+sigIndex <- which(p.adjust(ff$f_pval, 'bonf') < 0.05)
+
+## DERs from ER-level
+ders <- fullRegionGR[sigIndex]
+
+## Save DERs
+export(ders, 'BrainSpan_ER_DERs_cutoff_0.25.bed', format = 'BED')
 
 
 ## Reproducibility info
